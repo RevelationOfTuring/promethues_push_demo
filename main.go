5f98419c10a8531906f4cfd1917619585d6747e2
MichaelWang <michael_wangguanyi@163.com>
2023-06-19 11:09:37 +0800

    upd demo

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var (
+	// demo metric
+	cpuUsage = promauto.NewGauge(prometheus.GaugeOpts{
+		// metric name
+		Name: "cpu_usage",
+		// provides information about this metri
+		Help: "Current usage of the CPU.",
+	})
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			cpuUsage.Set(r.Float64())
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+func main() {
+	recordMetrics()
+	// NOTE: /metrics is fixed for prometheus pull, keep it fixed
+	http.Handle("/metrics", promhttp.Handler())
+	fmt.Println("====================== start metrics =================")
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		log.Fatal(err)
+	}
+}
